f4095932233d3bb8876cca180456fbd55005074e
etEthiopia <daginegussu@gmail.com>
2020-01-29 14:18:33 +0300

    R C D

--- routes/api/items.js
+++ routes/api/items.js
@@ -7,22 +7,47 @@ const Item = require("../../models/Item");
 // @desc GET All Items
 // @access Public
 router.get("/", async (req, res) => {
-  await Item.find()
-    .then(items => res.json(items))
-    .catch(err => res.json(res.status + ", Error: " + err));
+    await Item.find()
+        .then(items => res.json(items))
+        .catch(err => res.json({
+            Error: err,
+            success: false
+        }))
 });
 
 // @Route POST api/items
 // @desc Create an Item
 // @access Public
 router.post("/", async (req, res) => {
-  const newItem = new Item({
-    name: req.body.name
-  });
-  await newItem
-    .save()
-    .then(item => res.json(item))
-    .catch(err => res.json(res.status + ", Error: " + err));
+    const newItem = new Item({
+        name: req.body.name
+    });
+    await newItem
+        .save()
+        .then(item => res.json(item))
+        .catch(err => res.json({
+            Error: err,
+            success: false
+        }))
+});
+
+// @Route DELETE api/items
+// @desc Delete an Item
+// @access Public
+router.delete("/:id", async (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => {
+            res.json({
+                success: true
+            })
+        }).catch(err => res.json({
+            Error: err,
+            success: false
+        })))
+        .catch(err => res.json({
+            Error: err,
+            success: false
+        }))
 });
 
 module.exports = router;
