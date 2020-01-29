8d5e3d5289ad4e3168db5dab5704bf205aa7df16
etEthiopia <daginegussu@gmail.com>
2020-01-29 13:59:01 +0300

    post Item

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,14 +1,27 @@
 const router = require("express").Router();
+
 // Item Model
 const Item = require("../../models/Item");
 
 // @Route GET api/items
 // @desc GET All Items
 // @access Public
-
 router.get("/", async (req, res) => {
   await Item.find()
     .then(items => res.json(items))
+    .catch(err => res.json(res.status + ", Error: " + err));
+});
+
+// @Route POST api/items
+// @desc Create an Item
+// @access Public
+router.post("/", async (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+  await newItem
+    .save()
+    .then(item => res.json(item))
     .catch(err => res.json(res.status + ", Error: " + err));
 });
 
