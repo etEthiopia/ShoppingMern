30815bc909f112b9f23108e9c316c67b6950f1ba
etEthiopia <daginegussu@gmail.com>
2020-01-31 08:41:11 +0300

    ItemModal Change

---
+++ client/src/components/ItemModal.js
@@ -0,0 +1,29 @@
+import React, {
+    Component
+} from 'react';
+
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from 'react-bootstrap';
+
+import {connect} from 'react-redux'
+import {addItem} from '../actions/itemActions'
+
+
+class ItemModal extends Component {
+    state = {}
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default connect()(ItemModal);
