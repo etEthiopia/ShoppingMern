4fc831162c92871fcbb5d1922c5436bb4c99ed23
etEthiopia <daginegussu@gmail.com>
2020-01-31 14:48:50 +0300

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const path = require('path');
 //Import Routes
 const items = require("./routes/api/items");
 
@@ -29,6 +29,16 @@ try {
 
 // Configure Routes
 app.use("/items/", items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000;
 
