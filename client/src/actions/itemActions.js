66987bd70fd13d902522848d0810a5607e57619d
etEthiopia <daginegussu@gmail.com>
2020-01-31 14:17:55 +0300

    Basic Shopping API finished including DELETE

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -31,11 +31,12 @@ export const addItem = item => dispatch => {
         )
 };
 
-export const deleteItem = id => {
-    return {
-        type: DELETE_ITEM,
-        payload: id
-    };
+export const deleteItem = id => dispatch => {
+    axios.delete('/items/' + id).then(res =>
+        dispatch({
+            type: DELETE_ITEM,
+            payload: id
+        }))
 };
 
 
