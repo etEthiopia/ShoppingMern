89cf5b6c7af6ceebfc1879a126adf10a125eaad1
etEthiopia <daginegussu@gmail.com>
2020-02-15 20:58:12 +0300

    Reducers added in index.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,7 +2,11 @@ import {
     combineReducers
 } from 'redux';
 import ItemReducer from './ItemReducer';
+import ErrorReducer from './ErrorReducer';
+import AuthReducer from './AuthReducer';
 
 export default combineReducers({
-    item: ItemReducer
+    item: ItemReducer,
+    error: ErrorReducer,
+    Auth: AuthReducer
 });
