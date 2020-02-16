baa390b661563af6491bdd83a018c2d1061ebcd6
etEthiopia <daginegussu@gmail.com>
2020-02-16 16:56:05 +0300

    Auth Reducer Token Save on Login/Register Success

--- client/src/reducers/AuthReducer.js
+++ client/src/reducers/AuthReducer.js
@@ -32,6 +32,7 @@ export default function (state = initialState, action) {
             };
         case LOGIN_SUCCESS:
         case REGISTER_SUCCESS:
+            localStorage.setItem('shopingtoken', action.payload.token)
             return {
                 ...state,
                 ...action.payload,
