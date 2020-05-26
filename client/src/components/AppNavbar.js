bf37b39f7c9bea6571a0bd62877da82dee9e2f60
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-05-26 13:53:03 +0300

    See the change

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -59,7 +59,7 @@ class AppNavbar extends Component {
       <div>
         <Navbar color="dark" dark expand="sm" className="mb-5">
           <Container>
-            <NavbarBrand href="/">Shopping</NavbarBrand>
+            <NavbarBrand href="/">Shopping Mern</NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
